3c40877ea2850f7ac4e57dcb0c2061dd781b397e
dxrkness1337 <erqwe450@gmail.com>
2025-03-04 20:27:19 +0200

    My first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Передані аргументи командного рядка:");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Аргумент " + i + ": " + args[i]);
+        }
     }
 }
